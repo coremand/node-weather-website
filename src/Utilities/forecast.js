e6bfcff0af00523481b362e14e1057e35a5b549e
val del <valinevite@gmail.com>
2019-06-24 09:56:02 -0600

    changes to forecast

--- src/Utilities/forecast.js
+++ src/Utilities/forecast.js
@@ -9,7 +9,8 @@ const forecast = (longitude,latitude, callback) => {
          }else if(body.error) {
             callback("Invalid entry")
          }else {
-             callback(undefined, body.daily.data[0].summary + "It is currently" + " " + body.currently.temperature + " degrees out. "+ "There is a " + body.currently.precipProbability+"% " + " chance of rain.")
+             callback(undefined, body.daily.data[0].summary + " It is currently" + " " + body.currently.temperature +" degrees out with a high of " + body.daily.data[0].temperatureHigh + 
+             " degrees " + "and a low of " + body.daily.data[0].temperatureLow + " degrees . There is a " + body.currently.precipProbability+"% " + " chance of rain.");
          }
     });
 
